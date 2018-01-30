457f71a6c283ce7816abf87af85e0007dba919ce
hexidian <leokastenberg@gmail.com>
2018-01-29 21:12:55 -0500

    last version didn't actually work, but I fixed it

--- binary_word_generator.py
+++ binary_word_generator.py
@@ -1,4 +1,7 @@
 import time
+f = open("common_words.txt","r")
+words = f.read().split()
+f.close()
 ranges = {
     "0":[chr(i) for i in range(97,110)],#a-m
     "1":[chr(i) for i in range(110,123)]#n-z
@@ -6,25 +9,18 @@ ranges = {
 def word_look(binary):
     length = len(binary)
     for word in words:
+        works = True
         if len(word)!=length:
             continue
         for i in range(len(word)):
             if word[i] not in ranges[binary[i]]:#if the letter is in the appropriate range
-                continue
-        return word
+                works = False
+                break
+        if works:
+            return word
     return False
-binary = raw_input("binary?\n>>>")
+#binary = raw_input("binary?\n>>>")
 start = time.time()
-if len(binary)!=7:
-    f = open("common_words.txt","r")
-    words = f.read().split()
-    f.close()
-    #print test_words("", len(binary), binary)
-    print word_look(binary)
-else:
-    f = open("seven_letter_words.txt","r")
-    words = f.read().split()
-    f.close()
-    #print test_words("", 7, binary)
-    print word_look(binary)
+for i in xrange(64):
+    print word_look("{0:b}".format(i))
 print "completed in:",time.time()-start
